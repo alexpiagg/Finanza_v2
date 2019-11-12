e6e279c468ba88477072804c0560defd81ec6a84
alexpiagg <alexpiagg@hotmail.com>
2019-11-12 19:52:44 -0300

    Ajustes para PWA e nomes autores

--- app/view/_templates/heade.php
+++ app/view/_templates/heade.php
@@ -4,11 +4,11 @@
 <head>
     
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="Dashboard">
     <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+    <meta name="theme-color" content="#002966">
 
     <title>Finanza</title>
     
--- public/index.php
+++ public/index.php
@@ -10,14 +10,6 @@
  * @license http://opensource.org/licenses/MIT MIT License
  */
 
-/**
- * Agora MINI trablaha com namespaces + composer's autoloader (PSR-4)
- *
- * @author Joao Vitor Dias <joaodias@noctus.org>
- *
- * Para mais informações sobre namespaces @see http://php.net/manual/en/language.namespaces.importing.php
- */
-
 // definir uma constante que mantenha o caminho da pasta do projeto, como "/var/www/html/".
 // DIRECTORY_SEPARATOR adiciona uma para o final do path
 define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
