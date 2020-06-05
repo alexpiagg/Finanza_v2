a344397bf363407fe2abe685bd920a82b4489fb5
alexpiagg <alexpiagg@hotmail.com>
2020-06-05 18:32:21 -0300

    Remodelando a tela principal

--- app/Controller/TimelineController.php
+++ app/Controller/TimelineController.php
@@ -3,69 +3,146 @@
 namespace Mini\Controller;
 
 use Mini\Model\Gasto;
+use Mini\Model\Receita;
+use Mini\Model\ContaPagar;
 use Mini\Libs\Utils;
 
 class TimelineController
 {
 
+    private $saldoConta;
+    private $totalReceita;
+    private $totalGasto;
+    private $totalDespesas;
+    private $listaTimeline;
+    
+
     public function index()
     {
-        $dataInicial = date('Y-m-d', strtotime('first day of this month'));
-        $dataFinal = date('Y-m-d', strtotime('last day of this month'));
-
-        $gasto = new Gasto();
-
-        $retornoView = $gasto->getTimeline($dataInicial, $dataFinal, 1);
-
-        $login = $_SESSION['LOGIN'];        
-        $saldoConta = Utils::formatarMoeda($login->valor);
-
         require APP . 'view/timeline/index.php';
     }
 
-    function geraTimeline($dados)
-    {
-        $idx = 0;
-        
-        foreach ($dados as $time) {
-            $box   = "";
+    function totalizador(){
+        //Variáveis iniciais
+        $dataInicial = date('Y-m-d', strtotime('first day of this month'));
+        $dataFinal = date('Y-m-d', strtotime('last day of this month'));
+        $id_conta = $_SESSION['LOGIN']->id_conta;
 
-            $seta = "fa-circle";
-            if ($time->tipo == "RECEITA") {
-                $seta = "fa-plus";
-            }
+        //Parâmetros para a query
+        $parametros = array(
+            'data_inicial' => $dataInicial,
+            'data_final' => $dataFinal,
+            'id_conta' => $id_conta
+        );
 
-            //Determinando se a caixa da timeline ficará na esq. ou direita        
-            if ($idx & 1) {
-                $box = '<li>
-                            <div class="timeline-badge">
-                            <a><i class="fa ' . $seta . '" id=""></i></a>
-                        </div>';
-            } else {
-                $box = '<li class="timeline-inverted">
-                        <div class="timeline-badge">
-                            <a><i class="fa ' . $seta . ' invert" id=""></i></a>
-                        </div>';
-            }
+        //Saldo conta corrente
+        $login = $_SESSION['LOGIN'];
+        $this->saldoConta = $login->valor;
 
-            $idx++;
+        /**  GASTOS **/
+        $gasto = new Gasto();
 
-            echo    $box .
-                '   <div class="timeline-panel">
-                            <div class="timeline-heading">
-                                <h4> ' . $time->tipo . ' </h4>                        
-                            </div>
-                            <div class="timeline-body">
-                                <p>'
-                                . $time->descricao . ' ( R$ ' . Utils::formatarMoeda($time->valor) . ' )' .                                
-                                '</p>
-                                
-                            </div>
-                            <div class="timeline-footer">
-                                <p class="text-right">' . date_format(date_create($time->data), 'd/m/Y') . '</p>
-                            </div>
-                        </div>
-                    </li>';
+        $this->listaTimeline = $gasto->getTimeline($dataInicial, $dataFinal, 1);
+
+        $retornoDados = $gasto->getAll($parametros);
+        $this->totalGasto = array_sum(array_column($retornoDados, 'valor'));
+
+        /**  RECEITAS **/
+        $receita = new Receita();
+
+        $retornoDados = $receita->getByFilter($parametros);
+        $this->totalReceita = array_sum(array_column($retornoDados, 'valor'));
+
+
+        /**  CONTA PAGAR **/
+        $contaPagar = new ContaPagar();
+
+        $listaContasPagar = $contaPagar->getByFilter(null, $_SESSION['LOGIN']->id_conta);
+
+        foreach($listaContasPagar as $row){
+            $this->totalDespesas += $row->valor * $row->quantidade;
         }
+        
+    }
+
+    function geraHome()
+    {
+        $this->totalizador();
+        $this->geraIndicadores();
+        $this->geraTimeline();
+    }
+
+    function geraIndicadores(){
+
+        echo 
+        
+        '   
+            <div class="row mtbox">
+
+                <div class="col-md-2 col-sm-2 col-md-offset-1 box0">
+                    <div class="box1">
+                        <span class="fa fa-plus-square"></span>
+                        <h3>'. Utils::formatarMoeda($this->totalReceita) .'</h3>
+                    </div>
+                        <p>Receitas</p>
+                </div>
+                <div class="col-md-2 col-sm-2 box0">
+                    <div class="box1">
+                        <span class="fa fa-minus-square"></span>
+                        <h3>'. Utils::formatarMoeda($this->totalGasto) .'</h3>
+                    </div>
+                        <p>Gastos Efetuados</p>
+                </div>
+                <div class="col-md-2 col-sm-2 box0">
+                    <div class="box1">
+                        <span class="fa fa-bank"></span>
+                        <h3>'. Utils::formatarMoeda($this->saldoConta) .'</h3>
+                    </div>
+                        <p>Em conta</p>
+                </div>
+                <div class="col-md-2 col-sm-2 box0">
+                    <div class="box1">
+                        <span class="fa fa-calendar"></span>
+                        <h3>'. Utils::formatarMoeda($this->totalDespesas) .'</h3>
+                    </div>
+                        <p>A Pagar</p>
+                </div>    
+            </div><!-- row mt --> '	;
+    }
+
+    function geraTimeline(){
+
+        echo ' <div class="col-lg-8 col-md-offset-1 ds">
+                <h2>        </h2>';
+
+        if ($this->listaTimeline == null){
+            return;
+        }
+
+        foreach ($this->listaTimeline as $detalhe){
+
+            $icon = " fa fa-minus";
+            if ($detalhe->tipo == "RECEITA") {
+                $icon = " fa fa-plus";
+            }
+
+            echo '                                
+                <div class="desc">
+                    <div class="thumb">
+                        <span class="badge bg-theme"><i class="' .$icon. '"></i></span>
+                    </div>';
+                   
+            echo '<div class="details"> '
+                            . '<p>' .  date( 'd-m-Y' , strtotime( $detalhe->data ) ) . '<br/>'
+                            . '<a href="#"></a> R$ ' . Utils::formatarMoeda($detalhe->valor) . '<br/>'
+                            .  $detalhe->tipo . ' - '
+                            .  $detalhe->descricao . '<br/>'
+                        . '</p>
+                    </div>
+                </div>  ';
+        }
+
+        echo '</div>';
+        
     }
 }
--- app/view/timeline/index.php
+++ app/view/timeline/index.php
@@ -7,13 +7,11 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="assets/css/timeline.css" rel="stylesheet">
-
 
 <h4>
 <?php 
 
-    echo "Seu Saldo em ". date('d-m-Y') . ": <b> R$ ". $saldoConta ."</b>"; 
+    echo "Hoje é: ". date('d-m-Y'); 
 
 ?>
 </h4>
@@ -23,7 +21,7 @@
 
 <?php
 
-    $this->geraTimeline($retornoView);
+    $this->geraHome();
     
 ?>
     
@@ -35,3 +33,4 @@
     </section>
     <!--main content end-->
 </section>
+
