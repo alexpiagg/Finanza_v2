43eeb39c42226239ad4dca63f056e962e955517b
alexpiagg <alexpiagg@hotmail.com>
2020-12-18 15:00:21 -0300

    Alterando descrição da tela sobre

--- app/view/sobre/index.php
+++ app/view/sobre/index.php
@@ -7,7 +7,7 @@
             <div class="row mt">
                 <div class="col-sm-8">
                     <p>
-                        FINANZA - É um Sistema Web desenvolvido pela Piagg Software - &copy para controle de despesas e fluxo financeiro pessoal.</br>
+                        FINANZA - É um Aplicativo Web desenvolvido pela Piagg Sistemas - &copy para controle de despesas e fluxo financeiro pessoal.</br>
                         Seu objeto é ser acessível, simples e multi-plataforma.</br>
                         <a href="#" target="_blank">Visite</a>
                     </p>
